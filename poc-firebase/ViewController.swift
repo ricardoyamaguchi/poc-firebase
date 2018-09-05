209f819c0b49344a4c49c0d7f88b71fc22c90aad
Ricardo Yamaguchi <ricardoy@ciandt.com>
2018-09-05 15:38:05 -0300

    Initial Commit

---
+++ poc-firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poc-firebase
+//
+//  Created by Ricardo Yamaguchi on 05/09/18.
+//  Copyright © 2018 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
